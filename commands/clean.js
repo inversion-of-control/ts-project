583a8986a27f3a1f61e166beeb6c436bf43b6ca7
John Gardiner <john.gardiner@algomi.com>
2016-10-20 08:27:15 +0100

    fixed clean command logger

--- commands/clean.js
+++ commands/clean.js
@@ -1,12 +1,12 @@
 var npmRun = require('npm-run');
 
-module.exports = function clean(entry) {
-	npmRun.exec('rimraf target', {cwd: __dirname}, logCommand);
-};
-
 function logCleanCommand(err, stdout, stderr) 
 {
 	console.log('clean:err = ' + err);
 	console.log('clean:stdout = ' + stdout);
 	console.log('clean:stderr = ' + stderr);
 }
+
+module.exports = function clean(entry) {
+	npmRun.exec('rimraf target', {cwd: __dirname}, logCleanCommand);
+};
